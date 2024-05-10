0ea080b64b6ffeb2bb81e7b05d1442b25f27da63
Jakub Cierocki <jcierocki@gmail.com>
2024-05-10 11:47:52 +0200

    type aliases, non state-modyfing operations validation, single output from .execute()

--- friendly_arrow_sql/connection_wrapper.py
+++ friendly_arrow_sql/connection_wrapper.py
@@ -1,15 +1,19 @@
 import pyarrow as pa
 import adbc_driver_manager.dbapi as adbc
-import os
 import sys
 
 from urllib.parse import urlparse, ParseResult
-from typing import Literal
+from typing import Literal, TypeAlias
 from types import ModuleType
 
 import database_operation as ops
 
 from utils import package_load_validate
+
+
+DbOperationError: TypeAlias = ops.DatabaseOperationError
+DbOperation: TypeAlias = ops.AbstractDatabaseOperation
+StateModifyingDbOperation: TypeAlias = ops.AbstractStateModifyingDatabaseOperation
 
 
 class MissingSchemaError(Exception):
@@ -20,10 +24,15 @@ class SettingSchemaError(NotImplementedError):
     pass
 
 
+class ArgumentError(ValueError):
+    pass
+
+
 class DatabaseConnection:
     __uri: ParseResult
     __driver: ModuleType
     __conn: adbc.Connection | None
+    database_name: str
     schema: str | None
 
     def get_uri(self, hide_password: bool = True) -> str:
@@ -47,6 +56,7 @@ class DatabaseConnection:
         package_load_validate(adbc_driver_name, "adbc_driver")
         self.__driver = sys.modules["adbc_driver"]
 
+        self.database_name = self.__uri.path.removeprefix('/')
         if schema is not None:  # makes no impact yet
             if sql_dialect_name != "postgresql":
                 SettingSchemaError("Setting a default schema for the whole transaction " +
@@ -72,20 +82,29 @@ class DatabaseConnection:
             return self.__driver.connect(self.get_uri(hide_password=False), autocommit=False)
         return self.__conn
 
-    def execute(self,
-                operations: ops.AbstractDatabaseOperation | list[ops.AbstractDatabaseOperation]
-                ) -> list[pa.Table | None]:
-        if not isinstance(operations, list):
+    @classmethod
+    def _validate_only_state_modifying_operations(cls, operations: list[DbOperation]) -> None:
+        if any(not isinstance(op_, StateModifyingDbOperation) for op_ in operations):
+            raise ArgumentError(
+                "Multiple operations transactions are supported only for StateModifyingDatabaseOperation")
+
+    def execute(self, operations: DbOperation | list[DbOperation]) -> pa.Table | None:
+        single_output = False
+        if isinstance(operations, list):
+            # TODO rethink whether such a validation is needed
+            # there's no point to chain multiple selects within a single transaction
+            self._validate_only_state_modifying_operations(operations)
+        else:
+            single_output = True
             operations = [operations]
 
         conn_ = self._get_connection()
-
         query_results: list[pa.Table | None] = []
         with conn_.cursor() as cur:
             for op_ in operations:
                 try:
                     out = op_.execute_with_cursor(cur)
-                except ops.DatabaseOperationError as e:
+                except DbOperationError as e:
                     conn_.rollback()
                     raise e
 
@@ -95,5 +114,6 @@ class DatabaseConnection:
         if self.__conn is None:
             conn_.close()
 
-        return query_results
-
+        if single_output:
+            # otherwise returning None by default
+            return query_results[0]
