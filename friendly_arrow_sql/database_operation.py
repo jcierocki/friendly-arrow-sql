eb847e270e2fca11f8fb4a148ef032f11a8970f9
Jakub Cierocki <jcierocki@gmail.com>
2024-05-10 11:50:55 +0200

    customizable insert mode for BulkInsertOperation

--- friendly_arrow_sql/database_operation.py
+++ friendly_arrow_sql/database_operation.py
@@ -2,6 +2,7 @@ import pyarrow as pa
 import adbc_driver_manager.dbapi as adbc
 
 from abc import ABC, abstractmethod
+from typing import Literal
 
 
 class DatabaseOperationError(Exception):
@@ -44,15 +45,21 @@ class BulkInsertOperation(AbstractStateModifyingDatabaseOperation):
     table_name: str
     data: pa.Table
     schema: str
+    mode: Literal['append', 'create', 'create_append', 'replace']
 
-    def __init__(self, table_name: str, data: pa.Table, schema: str | None = None):
+    def __init__(self,
+                 table_name: str,
+                 data: pa.Table,
+                 schema: str | None = None,
+                 mode: Literal['append', 'create', 'create_append', 'replace'] = "append"):
         self.table_name = table_name
         self.data = data
         self.schema = schema
+        self.mode = mode
 
     def execute_with_cursor(self, cursor: adbc.Cursor) -> None:
         try:
-            cursor.adbc_ingest(self.table_name, self.data, db_schema_name=self.schema, mode="append")
+            cursor.adbc_ingest(self.table_name, self.data, db_schema_name=self.schema, mode=self.mode)
         except Exception as e:
             schema_prefix = '' if self.schema is None else f"{self.schema}."
             raise DatabaseOperationError(f"Failed to bulk insert data into {schema_prefix}{self.table_name}") from e
