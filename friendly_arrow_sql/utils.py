c77d7a85e602de534497db02f1dcea03461bc53d
Jakub Cierocki <jcierocki@gmail.com>
2024-05-11 23:48:39 +0200

    removed unused function

--- friendly_arrow_sql/utils.py
+++ friendly_arrow_sql/utils.py
@@ -23,15 +23,3 @@ def package_load_validate(package_name: str, alias: str | None = None) -> None:
     import_as_name = package_name if alias is None else alias
     sys.modules[import_as_name] = module
     spec.loader.exec_module(module)
-
-
-def load_adbc_sql_driver(sql_dialect: str):
-    match sql_dialect:
-        case "postgresql":
-            import adbc_driver_postgresql.dbapi as adbc_driver
-        case "sqlite":
-            import adbc_driver_sqlite.dbapi as adbc_driver
-        case "flightsql":
-            import adbc_driver_flightsql.dbapi as adbc_driver
-        case _:
-            raise ModuleNotInstalledError(f"ADBC driver package 'adbc_driver_{sql_dialect}' not installed.")
