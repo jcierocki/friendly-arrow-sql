f406e43f1166775eeed97acfc745be9f0c4c64c2
Jakub Cierocki <jcierocki@gmail.com>
2024-05-13 11:54:35 +0200

    added UpdateDeleteOperation class for performing UPDATE and/or DELETE operations using custom queries

--- friendly_arrow_sql/database_operation.py
+++ friendly_arrow_sql/database_operation.py
@@ -1,6 +1,8 @@
 import warnings
+import re
 
 import pyarrow as pa
+import numpy as np
 import adbc_driver_manager.dbapi as adbc
 
 from abc import ABC, abstractmethod
@@ -80,6 +82,40 @@ class BulkInsertOperation(AbstractStateModifyingDatabaseOperation):
             raise DatabaseOperationError(f"Failed to bulk insert data into {schema_prefix}{self.table_name}") from e
 
 
+class UpdateDeleteOperation(AbstractStateModifyingDatabaseOperation):
+    query: str
+    data: pa.Table
+    auto_adjust_dialect: bool
 
+    def _adjust_query_sql_dialect(self, cursor: adbc.Cursor):
+        # this method adjusts the query and data column order if detects that the 'cursor' was created using SQLite
+        # connection (SQLite requires different placeholder types as well as the data to be initially sorted)
+        if cursor._conn.__class__.__name__ == "AdbcSqliteConnection":
+            self.schema = None
 
+            field_name_placeholders = re.findall(r"\$\d", self.query)
+            column_order = np.argsort(field_name_placeholders)
+
+            self.query = re.sub(r"\$\d", '?', self.query)
+            self.data = self.data.select(np.array(self.data.column_names)[column_order])
+
+    def __init__(self,
+                 query: str,
+                 data: pa.Table,
+                 auto_adjust_dialect: bool = True
+                 # set ^^^ to False if you use Postgres or you manually adjusted the query and the data to
+                 # fit SQLite syntax
+                 ):
+        self.query = query
+        self.data = data
+        self.auto_adjust_dialect = auto_adjust_dialect
+
+    def execute_with_cursor(self, cursor: adbc.Cursor) -> None:
+        if self.auto_adjust_dialect:
+            self._adjust_query_sql_dialect(cursor)
+
+        try:
+            cursor.executemany(self.query, self.data)
+        except Exception as e:
+            raise DatabaseOperationError("Failed to execute query: \n" + self.query) from e
 
